5cb6be6889eceaeddaf1a6ca85d0008ca0372d80
ben10512 <>
2022-03-07 04:00:14 +0000

    pushing ch1 hello_wolrd.py

---
+++ hello_world.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+print("This is a hello world")
+
+my_string = "welcome to python 101"
+
+print(my_string)
+
+another_string_declaration = 'testing string 2'
+
+print(another_string_declaration)
+
+multi_line = '''Line 1
+line 2
+line 3
+option4'''
+
+print(multi_line)
+
+num123 = 123
+string123 = str(num123)
+print(string123)
+
+#concatinate string
+
+str1 = "fox jumped over the fence"
+str2 = "the hare ran away from the fox"
+
+str3 = str1 + str2
+
+print(str3)
+
+# String Methods
+all_caps_str = "all lower case string"
+print(all_caps_str.upper())
+
+capitalize_str = "cap the first word"
+print(capitalize_str.capitalize())
+
+str_slice = "All day long I party"
+print(str_slice[2:9])
+
